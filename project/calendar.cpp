6fd7ab8c192b9ece5a59e39ae5a056b56896308f
lixworth <lixworth@gmail.com>
2023-12-03 22:36:33 +0800

    feat: update

--- project/calendar.cpp
+++ project/calendar.cpp
@@ -1,8 +1,8 @@
 //
 // Created by Worth Lix on 2023/11/26 19:13.
 //
-
-/*第一次考核项目
+/*
+第一次考核项目
 制作一个万年历
 有如下功能：
 1.查询某年某月某日的日期是周几，该年是否为闰年
@@ -10,10 +10,12 @@
 3.需要一个菜单选择功能
 4.年份数据范围在公元后
 5.一个防报错功能，用户输出错误信息时能够提醒用户并重新输入
-6.可以自行添加细节*/
+6.可以自行添加细节
+*/
 #include <cstdio>
 #include <ctime>
 #include <iostream>
+#include <string>
 #include <vector>
 
 using namespace std;
@@ -31,23 +33,7 @@ using namespace std;
 
 vector<string> stringStream;
 
-void menu() {
-  LOG_CLEAR;
-  time_t now = time(0);
-  struct tm *local = localtime(&now);
-
-  printf("现在的时间是: %d/%02d/%02d %02d:%02d:%02d\n\n菜单:\n",
-         1900 + local->tm_year, 1 + local->tm_mon, local->tm_mday,
-         local->tm_hour, local->tm_min, local->tm_sec);
-  WARN << "1.查询对应年份的日历" << endl;
-  WARN << "2.查询对应日期的星期" << endl;
-  WARN << "3.查询对应年份是否为闰年" << endl;
-  WARN << "4.退出" << endl;
-  ERROR << "Version: 能用就行" << endl;
-
-  INFO << endl << "请输入菜单编号(1-4): ";
-}
-int get_week(int y,int m,int d) {
+int get_week(int y, int m, int d) {
   if (m == 1 || m == 2) {
     m += 12;
     y--;
@@ -55,12 +41,10 @@ int get_week(int y,int m,int d) {
   return ((d + 2 * m + 3 * (m + 1) / 5 + y + y / 4 - y / 100 + y / 400) % 7) +
          1;
 }
-
 int check_leap_year(int year) {
   return (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
 }
-
-int get_month_days_count(int year,int month) {
+int get_month_days_count(int year, int month) {
   // 1 3 5 7 8 10 12
   if (month == 2) {
     if (check_leap_year(year)) {
@@ -77,6 +61,23 @@ int get_month_days_count(int year,int month) {
   }
   return 30;
 }
+
+void menu() {
+  LOG_CLEAR;
+  time_t now = time(0);
+  struct tm *local = localtime(&now);
+
+  printf("现在的时间是: %d/%02d/%02d %02d:%02d:%02d\n\n菜单:\n",
+         1900 + local->tm_year, 1 + local->tm_mon, local->tm_mday,
+         local->tm_hour, local->tm_min, local->tm_sec);
+  WARN << "1.查询对应年份的日历" << endl;
+  WARN << "2.查询对应日期的星期" << endl;
+  WARN << "3.查询对应年份是否为闰年" << endl;
+  WARN << "4.退出" << endl;
+
+  INFO << endl << "请输入菜单编号(1-4): ";
+}
+
 void query_year() {
   cout << "请输入要查询的年份:" << endl;
   int query;
@@ -92,24 +93,29 @@ void query_year() {
   string cache;
 
   string echo_month[100][100] = {};
-  int echo_month_line = 0;
+  int echo_month_line;
 
   switch (input) {
   case 1:
     LOG_CLEAR;
-    SUCCESS << "====================== " << query  << "年 ======================" <<endl;
+    if (query == 1582) {
+      ERROR << "不支持该年份的查询! 震惊派蒙一整年.jpg" << endl;
+      break;
+    }
+    SUCCESS << "====================== " << query
+            << "年 ======================" << endl;
     for (int i = 1; i <= 12; ++i) {
       cout << RED << i << "月" << endl;
       cout << RESET << "周日\t周一\t周二\t周三\t周四\t周五\t周六" << endl;
       int start = get_week(query, i, 1);
-      if(start < 7){
+      if (start < 7) {
         for (int j = 0; j < start; ++j) {
           cout << "\t";
         }
       }
       for (int j = 1; j <= get_month_days_count(query, i); ++j) {
         printf("%02d\t", j);
-        if (get_week(query,i,j) == 6) {
+        if (get_week(query, i, j) == 6) {
           printf("\n");
         }
       }
@@ -118,10 +124,15 @@ void query_year() {
     break;
   case 2:
     LOG_CLEAR;
-    SUCCESS << "====================== " << query  << "年 ======================" <<endl;
-    for (int i = 1; i < 12; i=i+2) {
-      cache +=
-          RED + to_string(i) + "月" + "\t\t\t\t\t\t\t\t" + to_string(i+1) + "月";
+    if (query == 1582) {
+      ERROR << "不支持该年份的查询! 震惊派蒙一整年.jpg" << endl;
+      break;
+    }
+    SUCCESS << "====================== " << query
+            << "年 ======================" << endl;
+    for (int i = 1; i < 12; i = i + 2) {
+      cache += RED + to_string(i) + "月" + "\t\t\t\t\t\t\t\t" +
+               to_string(i + 1) + "月";
       stringStream.push_back(cache);
       cache.clear();
       cache = cache + RESET + "周日\t周一\t周二\t周三\t周四\t周五\t周六" +
@@ -129,29 +140,29 @@ void query_year() {
       stringStream.push_back(cache);
       cache.clear();
       int start = get_week(query, i, 1);
-      if(start < 7){
+      if (start < 7) {
         for (int j = 0; j < start; ++j) {
-          cache+="\t";
+          cache += "\t";
         }
       }
       echo_month_line = 0;
-      for (int j = 1; j <= get_month_days_count(query,i); ++j) {
-        if(j<10) {
+      for (int j = 1; j <= get_month_days_count(query, i); ++j) {
+        if (j < 10) {
           cache += to_string(0) + to_string(j) + "\t";
-        }else{
+        } else {
           cache += to_string(j) + "\t";
         }
-        if (get_week(query,i,j) == 6) {
+        if (get_week(query, i, j) == 6) {
           echo_month[i][echo_month_line] = cache;
           cache.clear();
           echo_month_line++;
         }
-        if(j == get_month_days_count(query,i)){
-          int week = get_week(query,i,j);
+        if (j == get_month_days_count(query, i)) {
+          int week = get_week(query, i, j);
           week = week == 7 ? 0 : week;
-          for (int k = 0; k < 7-week; ++k) {
-            if(k == 7-week-1){
-              cache +" ";
+          for (int k = 0; k < 7 - week; ++k) {
+            if (k == 7 - week - 1) {
+              cache + " ";
               continue;
             }
             cache += "  \t";
@@ -161,20 +172,21 @@ void query_year() {
         }
       }
       echo_month_line = 0;
-      start = get_week(query, i+1, 1);
-      if(start < 7){
+      start = get_week(query, i + 1, 1);
+      if (start < 7) {
         for (int j = 0; j < start; ++j) {
-          cache+="\t";
+          cache += "\t";
         }
       }
-      for (int j = 1; j <= get_month_days_count(query,i+1); ++j) {
-        if(j<10) {
+      for (int j = 1; j <= get_month_days_count(query, i + 1); ++j) {
+        if (j < 10) {
           cache += to_string(0) + to_string(j) + "\t";
-        }else{
+        } else {
           cache += to_string(j) + "\t";
         }
-        if (get_week(query,i+1,j) == 6 || j == get_month_days_count(query,i+1)) {
-          echo_month[i+1][echo_month_line] = cache;
+        if (get_week(query, i + 1, j) == 6 ||
+            j == get_month_days_count(query, i + 1)) {
+          echo_month[i + 1][echo_month_line] = cache;
           cache.clear();
           echo_month_line++;
         }
@@ -182,10 +194,10 @@ void query_year() {
       cache.clear();
 
       for (int j = 0; j < 6; ++j) {
-        if(echo_month[i][j] == ""){
-          cache = "  \t  \t  \t  \t  \t  \t  \t\t" + echo_month[i+1][j];
-        }else{
-          cache = echo_month[i][j] + "\t" + echo_month[i+1][j];
+        if (echo_month[i][j] == "") {
+          cache = "  \t  \t  \t  \t  \t  \t  \t\t" + echo_month[i + 1][j];
+        } else {
+          cache = echo_month[i][j] + "\t" + echo_month[i + 1][j];
         }
 
         stringStream.push_back(cache);
@@ -208,33 +220,36 @@ void query_week() {
   cout << "请输入要查询的日期: (年月日才用空格间隔)" << endl;
   int y, m, d;
   cin >> y >> m >> d;
+  if (y == 1582) {
+    ERROR << "不支持该年份的查询! 震惊派蒙一整年.jpg" << endl;
+  } else {
+    int week = get_week(y, m, d);
 
-  int week = get_week(y, m, d);
-
-  switch (week) {
-  case 1:
-    SUCCESS << "周一" << endl;
-    break;
-  case 2:
-    SUCCESS << "周二" << endl;
-    break;
-  case 3:
-    SUCCESS << "周三" << endl;
-    break;
-  case 4:
-    SUCCESS << "周四" << endl;
-    break;
-  case 5:
-    SUCCESS << "周五" << endl;
-    break;
-  case 6:
-    SUCCESS << "周六" << endl;
-    break;
-  case 7:
-    SUCCESS << "周日" << endl;
-    break;
-  default:
-    break;
+    switch (week) {
+    case 1:
+      SUCCESS << "周一" << endl;
+      break;
+    case 2:
+      SUCCESS << "周二" << endl;
+      break;
+    case 3:
+      SUCCESS << "周三" << endl;
+      break;
+    case 4:
+      SUCCESS << "周四" << endl;
+      break;
+    case 5:
+      SUCCESS << "周五" << endl;
+      break;
+    case 6:
+      SUCCESS << "周六" << endl;
+      break;
+    case 7:
+      SUCCESS << "周日" << endl;
+      break;
+    default:
+      break;
+    }
   }
 }
 void query_leap_year() {
@@ -242,13 +257,12 @@ void query_leap_year() {
   int year;
   cin >> year;
 
-  if(check_leap_year(year)){
+  if (check_leap_year(year)) {
     SUCCESS << "闰年" << endl;
-  }else{
+  } else {
     SUCCESS << "平年" << endl;
   }
 }
-
 
 int main() {
   int run = 1;
@@ -284,4 +298,3 @@ int main() {
   }
   return 0;
 }
-
