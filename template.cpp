5ac3e98c51a5fa6ae6b9309f41b72e30b7597ede
lixworth <lixworth@outlook.com>
2024-06-18 22:31:42 +0800

    updated on 2024-06-18 22:31

---
+++ template.cpp
@@ -0,0 +1,24 @@
+/*
+ * Created by LixWorth
+ * Github: https://github.com/lixworth/
+ * Website: https://blog.lix.moe/
+ * Contact: lixworth@outlook.com
+**/
+#include <bits/stdc++.h>
+#define endl "\n"
+#define IOS std::ios::sync_with_stdio(false),std::cin.tie(nullptr),std::cout.tie(nullptr);
+
+using namespace std;
+using ll = long long;
+
+void solve(){
+	cout << "Hello World" << endl;
+}
+
+int main(){
+	IOS
+	int tt = 1;
+	cin >> tt;
+	while(tt--) solve();
+	return 0;
+}
