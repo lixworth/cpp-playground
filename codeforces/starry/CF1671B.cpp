5ba6bb5865558cafcf20edbdbdd8595428902866
lixworth <lixworth@gmail.com>
2024-09-10 20:48:19 +0800

    updated on 2024-09-10 20:48

--- codeforces/starry/CF1671B.cpp
+++ codeforces/starry/CF1671B.cpp
@@ -3,13 +3,16 @@
  * Github: https://github.com/lixworth/
  * Website: https://blog.lix.moe/
  * Contact: lixworth@outlook.com
-**/
+ **/
 #include <bits/stdc++.h>
 
-#define FAST_IO std::ios::sync_with_stdio(false);std::cin.tie(nullptr);std::cout.tie(nullptr);
+#define FAST_IO                       \
+    std::ios::sync_with_stdio(false); \
+    std::cin.tie(nullptr);            \
+    std::cout.tie(nullptr);
 #define endl "\n"
 
-//using namespace std;
+// using namespace std;
 using std::cin, std::cout;
 using ll = long long;
 
@@ -19,17 +22,15 @@ int arr[N];
 void solve() {
     int n;
     cin >> n;
-    for (int i = 0; i < n; ++i) {
-        cin >> arr[i];
-    }
+    for (int i = 0; i < n; ++i) { cin >> arr[i]; }
 
     int cnt = 0; // 计算空隙 如果总空隙>2将无法移动
-    for (int i = 1; i < n; ++i) {
-        cnt += arr[i] - arr[i - 1] - 1;
-    }
+    for (int i = 1; i < n; ++i) { cnt += arr[i] - arr[i - 1] - 1; }
 
-    if (cnt > 2) cout << "NO" << endl;
-    else cout << "YES" << endl;
+    if (cnt > 2)
+        cout << "NO" << endl;
+    else
+        cout << "YES" << endl;
 }
 
 int main() {
--- template.cpp
+++ template.cpp
@@ -3,22 +3,22 @@
  * Github: https://github.com/lixworth/
  * Website: https://blog.lix.moe/
  * Contact: lixworth@outlook.com
-**/
+ **/
 #include <bits/stdc++.h>
-
 #define endl "\n"
-#define IOS std::ios::sync_with_stdio(false),std::cin.tie(nullptr),std::cout.tie(nullptr);
+#define IOS std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
 
 using namespace std;
 using ll = long long;
 
 void solve() {
-    cout << "Hello World" << endl;
+    int n;
+    cin >> n;
+    while (n--) cout << "Hello World!" << endl;
 }
 
 int main() {
-    IOS
-    int tt = 1;
+    IOS int tt = 1;
     cin >> tt;
     while (tt--) solve();
     return 0;
