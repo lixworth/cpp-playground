525f45aa89f44e1c81c541456dfc1ccd96cc9ff2
lixworth <lixworth@gmail.com>
2024-10-09 23:18:16 +0800

    updated on 2024-10-09 23:18

---
+++ problems/luogu/P1540.cpp
@@ -0,0 +1,46 @@
+/*
+ * Created by LixWorth
+ * Github: https://github.com/lixworth/
+ * Website: https://blog.lix.moe/
+ * Contact: lixworth@outlook.com
+ **/
+#include <bits/stdc++.h>
+#define endl "\n"
+#define IOS std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
+
+using namespace std;
+using ll = long long;
+
+const int N = 10000;
+int data[N];
+queue<int> mem;
+
+// https://www.luogu.com.cn/problem/P1540
+// 队列
+void solve() {
+    int m, n;
+    cin >> m >> n;
+    int cnt = 0;
+    for (int i = 0; i < n; ++i) {
+        int input;
+        cin >> input;
+        if (data[input] == 0) {
+            cnt++;
+            mem.push(input);
+            data[input] = 1;
+        }
+        if (mem.size() > m) {
+            data[mem.front()] = 0;
+            mem.pop();
+        }
+    }
+
+    cout << cnt << endl;
+}
+
+int main() {
+    IOS int tt = 1;
+    // cin >> tt;
+    while (tt--) solve();
+    return 0;
+}
