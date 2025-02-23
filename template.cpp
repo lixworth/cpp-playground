f31cd00c001b6a2b4a3bdad18014d36c558e6b7c
lixworth <lixworth@gmail.com>
2025-02-24 06:09:30 +0800

    updated on 2025-02-24 06:09

--- template.cpp
+++ template.cpp
@@ -1,9 +1,9 @@
 /*
- * Created by LixWorth
- * Github: https://github.com/lixworth/
- * Website: https://blog.lix.moe/
- * Contact: lixworth@outlook.com
- **/
+* Created by LixWorth
+* Github: https://github.com/lixworth/
+* Website: https://blog.lix.moe/
+* Contact: lixworth@outlook.com
+**/
 #include <bits/stdc++.h>
 #define endl "\n"
 #define IOS std::ios::sync_with_stdio(false), std::cin.tie(nullptr), std::cout.tie(nullptr);
@@ -14,8 +14,8 @@ using ll = long long;
 void solve() { cout << "Hello World!" << endl; }
 
 int main() {
-    IOS int tt = 1;
-    cin >> tt;
-    while (tt--) solve();
-    return 0;
+   IOS int tt = 1;
+   cin >> tt;
+   while (tt--) solve();
+   return 0;
 }
